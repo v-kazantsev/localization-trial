af722879b8c7491d3bdff5334d315c776c3ab3ef
v-kazantsev <vvs201@mail.ru>
2023-01-11 18:20:50 +0700

    fix types

--- lib/src/api/network_manager.dart
+++ lib/src/api/network_manager.dart
@@ -4,9 +4,9 @@ import '../models/codes_model.dart';
 import 'package:flutter/services.dart';
 
 class NetworkManager {
-  static Future<dynamic> readJson() async {
+  static Future<List<CodesModel>> readJson() async {
     final String response = await rootBundle.loadString('assets/data/mockData.json');
-    Iterable data = await json.decode(response);
+    final List<dynamic> data = await json.decode(response);
     return data.map((item) => CodesModel.fromJson(item)).toList();
   }
 }
